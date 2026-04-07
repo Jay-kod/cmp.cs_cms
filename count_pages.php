11b63f3b98e954a69eac711f59609ccb818f16cc
Jay-kod <jay-kod@users.noreply.github.com>
2026-04-07 15:10:10 +0100

    🗑️ remove: delete 2 files

--- count_pages.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-echo "Total dynamic pages in database: " . \App\Models\Page::count() . "\n";
