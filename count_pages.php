8e21deb2d97edb74eeeeb3f9621ef866e28e58de
Jay-kod <jay-kod@users.noreply.github.com>
2026-04-07 15:09:41 +0100

    📦 update: 2 files changed

---
+++ count_pages.php
@@ -0,0 +1,6 @@
+<?php
+require 'vendor/autoload.php';
+$app = require_once 'bootstrap/app.php';
+$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+echo "Total dynamic pages in database: " . \App\Models\Page::count() . "\n";
