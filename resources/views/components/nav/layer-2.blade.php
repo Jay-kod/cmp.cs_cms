09b2add4ea286c19a943e88239da0d3487fcdc81
Jay-kod <jay-kod@users.noreply.github.com>
2026-04-07 14:39:25 +0100

    📦 update: 5 files changed

--- resources/views/components/nav/layer-2.blade.php
+++ resources/views/components/nav/layer-2.blade.php
@@ -146,7 +146,7 @@
                 <div class="mobile-details-menu">
                     <a href="{{ url('/research-news') }}" class="mobile-link mobile-sub-link">News &amp; Announcements</a>
                     <a href="{{ url('/events') }}" class="mobile-link mobile-sub-link">Events &amp; Seminars</a>
-                    <a href="{{ url('/academic-calendar') }}" class="mobile-link mobile-sub-link">Academic Calendar</a>
+                    <a href="{{ route('page.show', 'academic-calendar') }}" class="mobile-link mobile-sub-link">Academic Calendar</a>
                 </div>
             </details>
 
