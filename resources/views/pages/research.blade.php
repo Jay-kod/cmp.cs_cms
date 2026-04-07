d91e201243c8442b5bc63ac7ff75097d9958636f
Jay-kod <jay-kod@users.noreply.github.com>
2026-04-08 00:30:41 +0100

    📦 update: 5 files changed

--- resources/views/pages/research.blade.php
+++ resources/views/pages/research.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.public')
-@section('title', 'Research & News')
+@section('title', 'Research & Innovations')
 
 @section('content')
 @php
@@ -15,19 +15,22 @@
 <div class="container page-layout" style="margin-top: -3rem; position: relative; z-index: 20; padding-bottom: 4rem;">
     <div class="main-content blog-main" style="background: white; border-radius: 16px; box-shadow: 0 20px 50px -12px rgba(0,0,0,0.1); padding: 3rem 4rem;">
 
-        @include('pages.research-news-partials.news')
+        @include('pages.research-news-partials.research-areas')
+
+        @include('pages.research-news-partials.publications')
 
         @include('pages.research-news-partials.events')
 
         @include('pages.research-news-partials.gallery')
 
-        @include('pages.research-news-partials.research-areas')
-
-        @include('pages.research-news-partials.publications')
-
     </div>
 
-    @include('pages.research-news-partials.toc')
+    <x-sticky-toc :sections="[
+        'research-areas' => 'Core Research Areas', 
+        'publications' => 'Publications',
+        'events' => 'Events Calendar', 
+        'gallery' => 'Photo Gallery'
+    ]" />
 </div>
 
 @include('pages.research-news-partials.styles')
