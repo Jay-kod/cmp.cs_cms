72146a0391bf66b91dc0e586e15635fbaa3849a4
Jay-kod <jay-kod@users.noreply.github.com>
2026-04-07 14:52:43 +0100

    📦 update: 8 files changed

--- replace.ps1
+++
@@ -1,4 +0,0 @@
-$content = Get-Content 'resources/views/layouts/admin.blade.php' -Raw
-$replacement = Get-Content 'nav_replacement.txt' -Raw
-$newContent = [regex]::replace($content, '(?s)<nav.*?</nav>', $replacement)
-Set-Content -Path 'resources/views/layouts/admin.blade.php' -Value $newContent -NoNewline
