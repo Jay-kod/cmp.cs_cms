6b5f354e07be2c8d5d0a7f006df602a5faf80d8c
Jay-kod <jay-kod@users.noreply.github.com>
2026-04-08 01:27:48 +0100

    📦 update: 4 files changed

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$c = file_get_contents("resources/views/components/nav/layer-2.blade.php");
-$pattern = "/                    Contact Us\s*<\/a>\s*<span><\/span><span><\/span><span><\/span>\s*<\/button>/";
-$replacement = "                    Contact Us\n                </a>\n            </nav>\n            <button class=\"navbar-hamburger\" id=\"mobile-menu-btn\" aria-label=\"Toggle navigation\">\n                <span></span><span></span><span></span>\n            </button>";
-$c = preg_replace($pattern, $replacement, $c);
-file_put_contents("resources/views/components/nav/layer-2.blade.php", $c);
-?>
