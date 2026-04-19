ca695c43998685ae09fff3a26934770bcc300d1c
Jay-kod <jay-kod@users.noreply.github.com>
2026-04-19 01:41:31 +0100

    📦 update: 2 files changed

--- resources/views/pages/home-partials/hod-welcome.blade.php
+++ resources/views/pages/home-partials/hod-welcome.blade.php
@@ -21,7 +21,7 @@
         </div>
 
         <!-- HoD Photo Area -->
-        <div class="hod-photo-container shrink-0 self-center flex flex-col gap-10 w-full max-w-[380px] lg:max-w-[380px] md:max-w-[500px]">
+        <div class="hod-photo-container shrink-0 self-center flex flex-col gap-10 w-full max-w-[100%] sm:max-w-[420px] md:max-w-[500px] lg:max-w-[380px]">
             <div class="hod-photo relative w-full mx-auto">
                 <div class="absolute -inset-3 border-2 border-primary rounded-xl z-[1] max-md:hidden"></div>
                 <div class="absolute inset-3 bg-primary/10 rounded-xl z-[1] max-md:hidden"></div>
