db3ef5b81890f48b580768ff52ecfde13556ddcf
Jay-kod <jay-kod@users.noreply.github.com>
2026-04-20 02:27:49 +0100

    📦 update: 6 files changed

---
+++ del3.php
@@ -0,0 +1,8 @@
+<?php
+require 'vendor/autoload.php';
+$app = require_once 'bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->bootstrap();
+
+Illuminate\Support\Facades\DB::table('department_settings')->where('key', 'about_facilities')->delete();
+echo "Deleted old facilities setting from DB\n";
