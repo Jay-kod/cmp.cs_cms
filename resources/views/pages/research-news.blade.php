2255554c03188486d612fe0ea2f32f73dee42335
Jay-kod <jay-kod@users.noreply.github.com>
2026-05-01 20:39:08 +0100

    📦 update: 2 files changed

--- resources/views/pages/research-news.blade.php
+++ resources/views/pages/research-news.blade.php
@@ -38,9 +38,9 @@
     </div>
 </div>
 
-<div class="container page-layout -mt-16 relative z-20 pb-16 max-w-7xl">
+<div class="container page-layout -mt-16 relative z-20 pb-16 max-w-7xl px-3 sm:px-4">
 
-    <div class="main-content blog-main bg-white rounded-3xl shadow-[0_8px_30px_rgb(0,0,0,0.06)] border border-slate-100 p-6 sm:p-10 lg:p-16">
+    <div class="main-content blog-main bg-white rounded-2xl sm:rounded-3xl shadow-[0_8px_30px_rgb(0,0,0,0.06)] border border-slate-100 p-4 sm:p-10 lg:p-16">
         @include('pages.research-news-partials.news')
         
         @include('pages.research-news-partials.announcements')
